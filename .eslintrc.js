137e22026d546325c1a81a09064792f9360aeac0
Leonardo Tiago <pequenotiago2016@gmail.com>
2025-09-02 17:06:55 +0100

    Estrutura do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': 'off', // permite console.log (útil em API)
+    'class-methods-use-this': 'off', // não obriga usar "this" em classes
+    'import/prefer-default-export': 'off', // permite exportar só funções nomeadas
+    'comma-dangle': ['error', 'never'] // remove vírgula no fim de objetos/arrays
+  }
+};
